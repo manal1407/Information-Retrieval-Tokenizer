3f967cafa833d57e1758000e5f8d1d29826eb5fb
Miren Tanna <mirrent@gmail.com>
2015-02-23 20:43:43 -0600

    Corrected computation of stems

--- Homework1.java
+++ Homework1.java
@@ -10,14 +10,15 @@ public class Homework1 {
 
     static int countOfFiles=0;
     static TreeMap<String, Integer> tokens = new TreeMap<String, Integer>();
-    static TreeMap<String, Integer> stemTokens = new TreeMap<String, Integer>();
+    static TreeMap<String, Integer> stems = new TreeMap<String, Integer>();
+
     static int numberOfTokens=0;
     static int numberOfStems=0;
     static int avgFile=0;
+    static int avgStemFile= 0;
 
     public static void main(String args[]){
-//         String filePath = args[0].toString();
-        String filePath = "E:\\Sem 5\\Information Retrieval\\Homework\\Homework1\\Cranfield_Collection";
+         String filePath = args[0].toString();
 
         long startTime = Calendar.getInstance().getTimeInMillis();
 
@@ -39,7 +40,7 @@ public class Homework1 {
         System.out.println("Number of tokens that occur only once: " + countOnes(tokens));
    //     System.out.println("Number of files: " + countOfFiles);
         System.out.println("Number of average tokens per document: " + numberOfTokens/countOfFiles);
-        System.out.println("Number of average distinct tokens per file: " + avgFile/countOfFiles);
+        System.out.println("Number of average 'distinct' tokens per document: " + avgFile/countOfFiles);
         System.out.println("30 most frequent tokens: ");
 
         TreeMap<String, Integer> sortedTokens= sortDecreasing(tokens);
@@ -54,13 +55,13 @@ public class Homework1 {
         System.out.println("Time taken to acquire characteristics: " + (Calendar.getInstance().getTimeInMillis()-startTime) + "ms");
         System.out.println("**********************");
 
-        stemTokens = stemTokens(tokens);
-        System.out.println("Number of distinct stems: " + stemTokens.size());
-        System.out.println("Number of stems occurring only once: " + countOnes(stemTokens));
+        System.out.println("Number of distinct stems: " + stems.size());
+        System.out.println("Number of stems occurring only once: " + countOnes(stems));
         System.out.println("Number of average stems per document: " + numberOfStems/countOfFiles);
+        System.out.println("Number of average 'distinct' stems per document: " + avgStemFile/countOfFiles);
         System.out.println("30 most frequent stems: ");
 
-        TreeMap<String, Integer> sortedStemTokens= sortDecreasing(stemTokens);
+        TreeMap<String, Integer> sortedStemTokens= sortDecreasing(stems);
         Iterator<Map.Entry<String, Integer>> sortedStemIterator = sortedStemTokens.entrySet().iterator();
         count=1;
         while(count<=30){
@@ -71,25 +72,6 @@ public class Homework1 {
 
     }
 
-    private static TreeMap<String, Integer> stemTokens(TreeMap<String, Integer> tokens) {
-        TreeMap<String, Integer> stems = new TreeMap<String, Integer>();
-        Iterator<Map.Entry<String, Integer>> stemEntryIterator = tokens.entrySet().iterator();
-
-        Stemmer stemmer = new Stemmer();
-        while(stemEntryIterator.hasNext()){
-            Map.Entry<String, Integer> entry= stemEntryIterator.next();
-            char stemChars[] = entry.getKey().toCharArray();
-            stemmer.add(stemChars, stemChars.length);
-            stemmer.stem();
-            numberOfStems+= entry.getValue();
-            if(stems.get(stemmer.toString()) == null)
-                stems.put(stemmer.toString(), entry.getValue());
-            else
-                stems.put(stemmer.toString(), entry.getValue() + 1);
-        }
-
-        return stems;
-    }
 
     private static void scanFiles(String filePath) throws FileNotFoundException {
         File file = new File(filePath);
@@ -99,7 +81,6 @@ public class Homework1 {
         for(int i=0; i<listOfFiles.length; i++){
             if(listOfFiles[i].isFile()){
                 countOfFiles++;
-   //             if(i<5)
                     fetchWords(listOfFiles[i]);
             }
 
@@ -136,6 +117,8 @@ public class Homework1 {
     static void fetchWords(File file) throws FileNotFoundException {
         Scanner inputFile = new Scanner(file);
         TreeMap<String, Integer> avgPerFile = new TreeMap<String, Integer>();
+        TreeMap<String, Integer> avgStemPerFile= new TreeMap<String, Integer>();
+
         while(inputFile.hasNextLine()){
 
             String currentLine = inputFile.nextLine();
@@ -150,21 +133,44 @@ public class Homework1 {
                     if(currentTokenModified.equals(""))
                         continue;
                     else{
+                        //push tokens of entire collection into TreeMap here
                         numberOfTokens++;
                         if(tokens.get(currentTokenModified) == null)
                             tokens.put(currentTokenModified, 1);
                         else
                             tokens.put(currentTokenModified, tokens.get(currentTokenModified) + 1);
 
+                        //count distinct tokens in each document here
                         if(avgPerFile.get(currentTokenModified) == null)
                             avgPerFile.put(currentTokenModified, 1);
                         else
                             avgPerFile.put(currentTokenModified, avgPerFile.get(currentTokenModified) + 1);
+
+                        //find stems here
+                        Stemmer stemmer= new Stemmer();
+                        char stemChars[] = currentTokenModified.toCharArray();
+                        stemmer.add(stemChars, stemChars.length);
+                        stemmer.stem();
+
+                        //push stems of entire collection into TreeMap here
+                        numberOfStems++;
+                        if(stems.get(stemmer.toString()) == null)
+                            stems.put(stemmer.toString(), 1);
+                        else
+                            stems.put(stemmer.toString(), stems.get(stemmer.toString()) + 1);
+
+                        //count distinct stems here
+                        if(avgStemPerFile.get(stemmer.toString()) == null)
+                            avgStemPerFile.put(stemmer.toString(), 1);
+                        else
+                            avgStemPerFile.put(stemmer.toString(), avgStemPerFile.get(stemmer.toString()) + 1);
                     }
                 }
             }
         }
         avgFile+= avgPerFile.size();
+        avgStemFile+= avgStemPerFile.size();
         avgPerFile.clear();
+        avgStemPerFile.clear();
     }
 }
